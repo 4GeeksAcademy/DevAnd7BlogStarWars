55b6d55fe81bd705a05d3240fb6e6f9c70400913
Andri Arrieta <andriarrieta7@gmail.com>
2026-03-22 12:20:14 +0000

    peticion a las API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+	const { store, dispatch } = useGlobalReducer()
 
-  const [people, setPeople] = useState([]);
+	const [people, setPeople] = useState([]);
+
+	useEffect(() => {
+		const fetchPeople = async () => fetch("https://www.swapi.tech/api/people");
+
+		fetchPeople().then(response => response.json()).then(respjson => {
+			const respPeople =respjson.results;
+			setPeople(respPeople);
+		});
+	}, [])
+
+console.log(people);
+
 	return (
 		<div className="d-flex flex-column align-items-center">
 			<div>
-			<p>Aqui va el Carrusell con los personajes</p>
-			</div>
-			
-			<div>
-			<p>Aqui va el Carrusell con las armas</p>
+				<p>Aqui va el Carrusell con los personajes</p>
 			</div>
 
 			<div>
-			<p>Aqui va el Carrusell con los planetas</p>
+				<p>Aqui va el Carrusell con las armas</p>
+			</div>
+
+			<div>
+				<p>Aqui va el Carrusell con los planetas</p>
 			</div>
 		</div>
 	);
