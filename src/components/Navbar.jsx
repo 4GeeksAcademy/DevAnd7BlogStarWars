98a7e7b89e10705caed530b3f1a8eef4b30b007c
Andri Arrieta <andriarrieta7@gmail.com>
2026-03-24 08:33:04 +0000

    navbar logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,9 @@ export const Navbar = () => {
       <div className="container-fluid">
         <a className="navbar-brand text-white" href="#">
           <div className="d-flex">
-            <img
+            <img className="logotipo"
               src="https://logodownload.org/wp-content/uploads/2015/12/star-wars-logo-0.png"
-              alt="Star Wars"
-              style={{
-                height: "40px",
-                width: "100px",
-                objectFit: "cover"
-              }}
+              alt="Logo de Star Wars"
             />
             <h1>
               Blog de Star Wars
